aa5542c7eed6c313a8445b740a60dc4681be7d01
unknown <vigneshjanagam01@gmail.com>
2024-05-15 19:07:08 +0530

    update

---
+++ JS/first/script.js
@@ -0,0 +1,13 @@
+console.log("hello");
+var a=20;
+a=a+6
+console.log(a)
+alert("raj");
+confirm("are you sure")
+var val1 = prompt("enter the name")
+var val2 = prompt("enter the name")
+console.log(val1+val2);
+
+var b=20;
+console.log(b+1,typeof(b))
+console.log(b.toFixed(3))
